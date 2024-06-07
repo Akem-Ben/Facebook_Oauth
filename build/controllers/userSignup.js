5a739dd703da506f10265b7bb9041c5ccc51e46f
Akem-Ben <akemini.ndaobong@gmail.com>
2024-06-07 18:37:08 +0100

    tests

--- build/controllers/userSignup.js
+++ build/controllers/userSignup.js
@@ -2,18 +2,75 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerUser = void 0;
 const app_1 = require("../app");
+const uuid_1 = require("uuid");
 const registerUser = async (request, response) => {
     try {
-        const { email, password } = request.body;
-        const { data, error } = await app_1.supabase.auth.signUp({
-            email: 'example@email.com',
-            password: 'example-password',
+        const { firstName, lastName, email, phone, password } = request.body;
+        if (!firstName || !lastName || !email || !phone) {
+            return response.status(400).json({
+                message: "All fields are required"
+            });
+        }
+        const { data: findAdmin, error: findAdminError } = await app_1.supabase
+            .from('users')
+            .select('*')
+            .eq('email', email)
+            .single();
+        if (findAdminError && findAdminError.code !== 'PGRST116') {
+            // 'PGRST116' is the error code for "no rows returned", ignore it if this is the case
+            return response.status(500).json({
+                message: "Internal Server Error"
+            });
+        }
+        if (findAdmin) {
+            return response.status(400).json({
+                message: "HR already exists"
+            });
+        }
+        const { data: saveAdmin, error: createAdminError } = await app_1.supabase
+            .from('users')
+            .insert({
+            id: (0, uuid_1.v4)(),
+            firstName,
+            lastName,
+            email,
+            phone,
+            workEmail: email,
+            password,
+            isManager: true,
+            designation: "HR"
+        });
+        if (createAdminError) {
+            return response.status(400).json({
+                message: "Unable to create, try again later"
+            });
+        }
+        const { data: checkAdmin, error: checkAdminError } = await app_1.supabase
+            .from('users')
+            .select('*')
+            .eq('email', email)
+            .single();
+        if (checkAdminError || !checkAdmin) {
+            return response.status(400).json({
+                message: "Unable to create, try again later"
+            });
+        }
+        return response.status(200).json({
+            message: "Admin Registered",
+            admin: {
+                firstName: checkAdmin.firstName,
+                lastName: checkAdmin.lastName,
+                email: checkAdmin.email,
+                workEmail: checkAdmin.workEmail,
+                phone: checkAdmin.phone,
+                designation: checkAdmin.designation
+            }
         });
     }
     catch (error) {
         console.log(error.message);
-        return response.status(500).json({
-            message: `Internal Server Error, Please contact the admin`
+        response.status(500).json({
+            message: "Internal Server Error"
         });
     }
 };
